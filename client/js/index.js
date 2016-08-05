f3e375e0116a1a20031fbef8ab223e20be82dccb
Márton Borlay <borlaym@gmail.com>
2016-08-05 11:10:52 +0200

    Added socketio

--- client/js/index.js
+++ client/js/index.js
@@ -2,4 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 
+import io from 'socket.io-client';
+
+let socket = io('http://localhost:3001');
+
+socket.on('connect', (socket) => {
+	alert(111)
+});
+
 ReactDOM.render(<App />, document.getElementById('main'));
