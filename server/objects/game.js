292b63259e0d89fc3a204992378fedcebf508a78
Márton Borlay <borlaym@gmail.com>
2016-08-05 15:13:19 +0200

    Kokapirollo server

--- server/objects/game.js
+++ server/objects/game.js
@@ -1,26 +1,60 @@
 'use strict';
 
+let resolver = {
+	'kopapir': 1,
+	'koollo': 0,
+	'papirko': 0,
+	'papirollo': 1,
+	'ollopapir': 0,
+	'olloko': 1
+};
+
 class Game {
 	constructor(players) {
 		this.id = Math.floor(Math.random() * 900000);
 		this.players = players;
 		this.players.map(player => player.socket).forEach(socket => socket.join(this.id));
-		this.broadcast('gameStart', {});
-		this.on('msg', this.foo.bind(this));
+		this.events = {
+			'action': 'onAction'
+		};
+		Object.keys(this.events).forEach(key => {
+			this.on(key, this[this.events[key]].bind(this));
+		});
+		this.broadcast('gameStart');
+		this.actions = [];
 	}
 	broadcast(name, payload) {
 		this.players.map(player => player.socket).forEach(socket => socket.emit(name, payload));
 	}
 	on(name, callback) {
-		this.players.map(player => player.socket).forEach(socket => socket.on(name, callback));
+		this.players.map(player => player.socket).forEach(socket => {
+			socket.on(name, param => {
+				callback(param, socket.player);
+			});
+		})
 	}
-	foo(payload) {
-		if (this.partialMsg) {
-			this.broadcast('returnMsg', payload + this.partialMsg);
-			this.partialMsg = null;
-		} else {
-			this.partialMsg = payload;
+	serialize() {
+		return {
+			players: this.players.map(player => player.serialize())
 		}
+	}
+	onAction(type, player) {
+		this.actions.push({
+			player,
+			type
+		});
+		if (this.actions.length === 2) {
+			this.resolveActions();
+		}
+	}
+	resolveActions() {
+		let winner = resolver[this.actions.map(action => action.type).join()];
+		winner = actions[winner].serialize();
+		this.broadcast('roundEnd', {
+			actions: this.actions,
+			winner
+		});
+		this.actions = [];
 	}
 }
 
--- server/objects/player.js
+++ server/objects/player.js
@@ -6,11 +6,17 @@ module.exports =  class Player {
 			addName: 'addName'
 		};
 		this.socket = socket;
+		this.socket.player = this;
 		Object.keys(this.events).forEach(key => {
 			this.socket.on(key, this[this.events[key]].bind(this));
 		});
 	}
 	addName(name) {
 		this.name = name;
+	}
+	serialize() {
+		return {
+			name: this.name
+		}
 	}
 }
