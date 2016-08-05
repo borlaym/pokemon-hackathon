72d0ca5745db4606445d38f1f77c24479aae5037
Márton Borlay <borlaym@gmail.com>
2016-08-05 11:31:44 +0200

    rooms

--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
+'use strict';
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+let waitingPlayer;
+let rooms = [];
+
 io.on('connection', function(socket){
-  console.log('a user connected');
+  if (waitingPlayer) {
+		let newRoom = {
+			id: Math.floor(Math.random() * 900000),
+			players: [socket, waitingPlayer]
+		};
+		rooms.push(newRoom);
+		waitingPlayer = null;
+		console.log('created new room');
+	} else {
+		waitingPlayer = socket;
+		console.log('waiting for other player');
+	}
+	socket.on('disconnect', () => {
+		if (waitingPlayer === socket) {
+			waitingPlayer = null;
+		}
+	})
 });
 
 http.listen(3001, function(){
