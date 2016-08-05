21cabb4245e762be5bdda386f892482e70349aad
Gáspár Körtesi <gazs@gawker.com>
2016-08-05 21:13:31 +0200

    add textview, ttf pixelfont

--- client/js/views/LoginView.js
+++ client/js/views/LoginView.js
@@ -1,6 +1,8 @@
 import Backbone from 'backbone';
 import EventBus from '../eventBus.js';
 import PokemonView from '../views/PokemonView.js';
+import TextView from '../views/TextView.js';
+
 let LoginView = Backbone.View.extend({
 	events: {
 		'submit form': 'onSubmit'
@@ -10,11 +12,13 @@ let LoginView = Backbone.View.extend({
 		EventBus.trigger('connect', this.$('input').val());
 	},
 	render() {
-		this.$el.html('<form><input type="text"><button type="submit">Search for opponent</button></form>');
+		//this.$el.html('<form><input type="text"><button type="submit">Search for opponent</button></form>');
 
-		this.$el.append(new PokemonView('opponent').render())
-		this.$el.append(new PokemonView('player').render())
+		//this.$el.append(new PokemonView('opponent').render())
+		//this.$el.append(new PokemonView('player').render())
 
+		this.$el.append(new TextView("Gyonyoru!!444Pikémon!").render())
+		
 		return this.$el;
 	}
 });
