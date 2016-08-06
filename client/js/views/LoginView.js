f27c96f5f4c635ffbecb0fa6d13eb47b081d60db
Márton Borlay <borlaym@gmail.com>
2016-08-06 06:48:42 +0200

    Searching for opponent

--- client/js/views/LoginView.js
+++ client/js/views/LoginView.js
@@ -4,15 +4,18 @@ import PokemonView from '../views/PokemonView.js';
 import TextView from '../views/Textview.js';
 
 let LoginView = Backbone.View.extend({
+	className: 'loginView',
 	events: {
 		'submit form': 'onSubmit'
 	},
 	onSubmit(event) {
 		event.preventDefault();
-		EventBus.trigger('connect', this.$('input').val());
+		EventBus.trigger('connect');
+		this.$('button').remove();
+		this.$el.append('<p>Searching for opponent...</p>');
 	},
 	render() {
-		this.$el.html('<form><input type="text"><button type="submit">Search for opponent</button></form>');
+		this.$el.html('<h4>Pokemon</h4><form><button type="submit">Search for opponent</button></form>');
 
 		return this.$el;
 	}
